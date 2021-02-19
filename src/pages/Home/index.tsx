a1e76e37964fa019e8b74bf15903f02c058eb8c9
murilolemes <murilo.lemes.mhl@gmail.com>
2021-02-19 18:14:46 -0300

    best services and Our Blog

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiSearch } from 'react-icons/fi';
+import { FaUser } from 'react-icons/fa';
 
 import food1 from '../../assets/comida_1.svg';
 import food2 from '../../assets/comida_2.svg';
 import food3 from '../../assets/comida_3.svg';
 import food4 from '../../assets/comida_4.svg';
+import blocoServicesImg from '../../assets/bloco_services.svg';
+import blogImg1 from '../../assets/blog_image_1.svg';
+import blogImg2 from '../../assets/bloco_image_2.svg';
+import blogImg3 from '../../assets/bloco_image_3.svg';
+import blogImg4 from '../../assets/bloco_image_4.svg';
 
 import {
   Container,
@@ -14,6 +20,8 @@ import {
   Header,
   NewRecipe,
   BestRecipes,
+  BestServices,
+  OurBlog,
 } from './styles';
 
 const Home: React.FC = () => (
@@ -24,11 +32,19 @@ const Home: React.FC = () => (
           <h1>Healthy Food</h1>
           <div>
             <ul>
-              <li>HEALTHY RECIPES</li>
-              <li>BLOG</li>
-              <li>JOIN</li>
               <li>
-                <Link to="/">REGISTER</Link>
+                <Link to="/">HEALTHY RECIPES</Link>
+              </li>
+              <li>
+                <Link to="/">BLOG</Link>
+              </li>
+              <li>
+                <Link to="/">JOIN</Link>
+              </li>
+              <li>
+                <Link to="/" id="register">
+                  REGISTER
+                </Link>
               </li>
             </ul>
           </div>
@@ -53,29 +69,29 @@ const Home: React.FC = () => (
             Vokalia and Consonantia, there live the blind texts.
           </p>
         </div>
-        <div id="divCards">
-          <div className="card">
+        <div id="divCardsRecipe">
+          <div className="cardRecipe">
             <img src={food1} alt="Food1" />
             <div>
               <p>Broccoli Salad with Bacon</p>
               <button type="button">See Recipe</button>
             </div>
           </div>
-          <div className="card">
+          <div className="cardRecipe">
             <img src={food2} alt="Food2" />
             <div>
               <p>Classic Beef Burgers</p>
               <button type="button">See Recipe</button>
             </div>
           </div>
-          <div className="card">
+          <div className="cardRecipe">
             <img src={food3} alt="Food3" />
             <div>
               <p>Classic Potato Salad</p>
               <button type="button">See Recipe</button>
             </div>
           </div>
-          <div className="card">
+          <div className="cardRecipe">
             <img src={food4} alt="Food4" />
             <div>
               <p>Cherry Cobbler on the Grill</p>
@@ -84,6 +100,92 @@ const Home: React.FC = () => (
           </div>
         </div>
       </BestRecipes>
+      <BestServices>
+        <img src={blocoServicesImg} alt="Services" />
+        <div>
+          <h2>The best services ready To serve you</h2>
+          <p>
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts.
+          </p>
+          <p>
+            Separated they live in Bookmarksgrove right at the coast of the
+            Semantics, a large language ocean.
+          </p>
+          <p>
+            A small river named Duden flows by their place and supplies it with
+            the necessary regelialia.
+          </p>
+          <button type="button">Know More</button>
+        </div>
+      </BestServices>
+      <OurBlog>
+        <div id="textOurBlog">
+          <h2>Read Our Blog</h2>
+          <p>
+            Far far away, behind the word mountains, far from the countries
+            Vokalia and Consonantia, there live the blind texts.
+          </p>
+        </div>
+        <div id="divCardsBlog">
+          <div className="cardBlog">
+            <Link to="/">
+              <img src={blogImg1} alt="Imagem Blog 1" />
+              <div className="descriptionCardBlog">
+                <p>Quick-start guide to nuts and seeds</p>
+                <div className="descriptionNameAvatar">
+                  <div className="imgAvatar">
+                    <FaUser size={30} />
+                  </div>
+                  <p>Kevin Ibrahim</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+          <div className="cardBlog">
+            <Link to="/">
+              <img src={blogImg2} alt="Imagem Blog 2" />
+              <div className="descriptionCardBlog">
+                <p>Nutrition: Tips for Improving Your Health</p>
+                <div className="descriptionNameAvatar">
+                  <div className="imgAvatar">
+                    <FaUser size={30} />
+                  </div>
+                  <p>Mike Jackson</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+          <div className="cardBlog">
+            <Link to="/">
+              <img src={blogImg3} alt="Imagem Blog 3" />
+              <div className="descriptionCardBlog">
+                <p>The top 10 benefits of eating healthy</p>
+                <div className="descriptionNameAvatar">
+                  <div className="imgAvatar">
+                    <FaUser size={30} />
+                  </div>
+                  <p>Bryan McGregor</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+          <div className="cardBlog">
+            <Link to="/">
+              <img src={blogImg4} alt="Imagem Blog 4" />
+              <div className="descriptionCardBlog">
+                <p>What Makes a Healthy Diet?</p>
+                <div className="descriptionNameAvatar">
+                  <div className="imgAvatar">
+                    <FaUser size={30} />
+                  </div>
+                  <p>Jashua</p>
+                </div>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </OurBlog>
     </Content>
   </Container>
 );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -43,16 +43,32 @@ export const Header = styled.div`
         }
 
         a {
-          width: 130px;
-          height: 52px;
           border: 0;
           text-decoration: none;
-          background: #fff;
-          color: #badc58;
+          color: #fff;
           border-radius: 5px;
           display: flex;
           align-items: center;
           justify-content: center;
+          transition: font-size 0.2s;
+
+          :hover {
+            font-size: 18px;
+          }
+        }
+
+        #register {
+          width: 130px;
+          height: 52px;
+          border: 0;
+          background: #fff;
+          color: #badc58;
+          border-radius: 5px;
+          transition: box-shadow 0.2s;
+
+          :hover {
+            box-shadow: 0 0 8px #fff;
+          }
         }
       }
     }
@@ -107,6 +123,11 @@ export const NewRecipe = styled.div`
         display: flex;
         align-items: center;
         justify-content: center;
+        transition: box-shadow 0.4s;
+
+        :hover {
+          box-shadow: 0 1px 15px #badc58;
+        }
 
         svg {
           width: 24px;
@@ -137,18 +158,18 @@ export const BestRecipes = styled.div`
 
     p {
       text-align: center;
-      line-height: 27px;
+      line-height: 26px;
     }
   }
 
-  #divCards {
+  #divCardsRecipe {
     display: flex;
     align-items: center;
     justify-content: center;
     flex-wrap: wrap;
     margin-top: 32px;
 
-    .card {
+    .cardRecipe {
       width: 528px;
       height: 226px;
       margin: 18px;
@@ -184,6 +205,159 @@ export const BestRecipes = styled.div`
           margin-top: 16px;
           background: #badc58;
           color: #fff;
+          transition: box-shadow 0.4s;
+
+          :hover {
+            box-shadow: 0 1px 15px #badc58;
+          }
+        }
+      }
+    }
+  }
+`;
+
+export const BestServices = styled.div`
+  height: 768px;
+  padding: 48px;
+  display: flex;
+  align-items: center;
+
+  div {
+    width: 398px;
+
+    h2 {
+      font-size: 32px;
+      line-height: 42px;
+      margin-bottom: 32px;
+    }
+
+    p {
+      line-height: 28px;
+      color: #bcbcbc;
+
+      & + p {
+        margin-top: 26px;
+      }
+    }
+
+    button {
+      width: 150px;
+      height: 52px;
+      margin-top: 48px;
+      border: 0;
+      border-radius: 5px;
+      background: #badc58;
+      color: #fff;
+      transition: box-shadow 0.4s;
+
+      :hover {
+        box-shadow: 0 1px 15px #badc58;
+      }
+    }
+  }
+`;
+
+export const OurBlog = styled.div`
+  width: 100%;
+  height: 768px;
+  padding: 20px 48px 48px 48px;
+  display: flex;
+  flex-direction: column;
+
+  border: 1px solid blue;
+
+  #textOurBlog {
+    width: 510px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h2 {
+      font-size: 32px;
+      line-height: 70px;
+    }
+
+    p {
+      text-align: center;
+      line-height: 26px;
+    }
+  }
+
+  #divCardsBlog {
+    display: flex;
+    align-items: center;
+    margin-top: 40px;
+    overflow-x: hidden;
+
+    border: 1px solid red;
+
+    .cardBlog {
+      width: 348px;
+      height: 450px;
+      border-radius: 7px;
+      box-shadow: 0 15px 20px;
+
+      & + .cardBlog {
+        margin-left: 24px;
+      }
+
+      a {
+        display: flex;
+        flex-direction: column;
+        text-decoration: none;
+        height: 100%;
+
+        img {
+          width: 348px;
+          height: 248px;
+        }
+
+        .descriptionCardBlog {
+          width: 100%;
+          height: 100%;
+          padding: 20px;
+          display: flex;
+          flex-direction: column;
+          align-items: center;
+          justify-content: space-between;
+
+          p {
+            font-family: 'Montserrat';
+            font-size: 24px;
+            font-weight: bold;
+            line-height: 34px;
+            color: #1d164d;
+          }
+
+          .descriptionNameAvatar {
+            width: 100%;
+            display: flex;
+            align-items: center;
+
+            .imgAvatar {
+              width: 52px;
+              height: 52px;
+              border-radius: 50%;
+              display: flex;
+              align-items: center;
+              justify-content: center;
+              box-shadow: 0 0 3px #bcbcbc;
+              margin-right: 10px;
+
+              svg {
+                color: #bcbcbc;
+                opacity: 0.5;
+              }
+            }
+
+            p {
+              font-family: 'Mulish';
+              font-size: 16px;
+              font-weight: 400;
+              line-height: 20px;
+              color: #9e9baf;
+            }
+          }
         }
       }
     }
